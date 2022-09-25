084c00b4321578e9396c5d685a5a8c000f2efdea
alemonacoo <ale.monaco@outlook.com>
2022-09-25 17:21:24 +0200

    Excercise Complete!

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="appTitle">
+    <h1>La mia prima app con Vue CLI!</h1>
+</div>
+</template>
+
+<script>
+    export default {
+      name: 'AppTitle',
+    }
+</script>
+
+<style scoped lang="scss">
+    h1{
+        font-family: Helvetica, sans-serif;
+        font-size: 3rem;
+        color: blue;
+    }
+</style>
+
